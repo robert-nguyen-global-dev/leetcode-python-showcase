1938d0f0b191ceb02dc202c4a06bf5acf908a386
Robert Nguyen <robert.nguyen.global.dev@gmail.com>
2025-07-25 22:34:53 +0700

    refactor(test-merge-sorted-array):migrate from unittest to pytest for cleaner assertions

--- easy/0088_merge_sorted_array/test_merge_sorted_array.py
+++ easy/0088_merge_sorted_array/test_merge_sorted_array.py
@@ -1,36 +1,32 @@
-import unittest
+import pytest
 from merge_sorted_array import Solution
 
 
-# 🧪 Unit tests for internal logic `_merge_sorted_array`
-class TestMergeSortedArray(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
+@pytest.fixture
+def solution():
+    return Solution()
+    
 
-    def test_case_1(self):
-        nums1 = [1, 2, 3, 0, 0, 0]
-        nums2 = [2, 5, 6]
-        self.solution._merge_sorted_array(nums1, 3, nums2, 3)
-        self.assertEqual(nums1, [1, 2, 2, 3, 5, 6])
+def test_case_1(solution):
+    nums1 = [1, 2, 3, 0, 0, 0]
+    nums2 = [2, 5, 6]
+    solution._merge_sorted_array(nums1, 3, nums2, 3)
+    assert nums1 == [1, 2, 2, 3, 5, 6]
 
-    def test_case_2(self):
-        nums1 = [1]
-        nums2 = []
-        self.solution._merge_sorted_array(nums1, 1, nums2, 0)
-        self.assertEqual(nums1, [1])
+def test_case_2(solution):
+    nums1 = [1]
+    nums2 = []
+    solution._merge_sorted_array(nums1, 1, nums2, 0)
+    assert nums1 == [1]
 
-    def test_case_3(self):
-        nums1 = [0]
-        nums2 = [1]
-        self.solution._merge_sorted_array(nums1, 0, nums2, 1)
-        self.assertEqual(nums1, [1])
+def test_case_3(solution):
+    nums1 = [0]
+    nums2 = [1]
+    solution._merge_sorted_array(nums1, 0, nums2, 1)
+    assert nums1 == [1]
 
-    def test_case_4(self):
-        nums1 = [2, 0]
-        nums2 = [1]
-        self.solution._merge_sorted_array(nums1, 1, nums2, 1)
-        self.assertEqual(nums1, [1, 2])
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_case_4(solution):
+    nums1 = [2, 0]
+    nums2 = [1]
+    solution._merge_sorted_array(nums1, 1, nums2, 1)
+    assert nums1 == [1, 2]
